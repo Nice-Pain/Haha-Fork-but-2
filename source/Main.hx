8c3ec386996afa5de3f781da63d4576229030d3c
Nice-Pain <107368503+Nice-Pain@users.noreply.github.com>
2022-12-23 20:12:00 +0000

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -10,16 +10,17 @@ import openfl.display.FPS;
 import openfl.display.Sprite;
 import openfl.events.Event;
 import openfl.display.StageScaleMode;
-
-//crash handler stuff
-#if CRASH_HANDLER
 import lime.app.Application;
-import openfl.events.UncaughtErrorEvent;
-import haxe.CallStack;
-import haxe.io.Path;
+
 #if desktop
 import Discord.DiscordClient;
 #end
+
+//crash handler stuff
+#if CRASH_HANDLER
+import openfl.events.UncaughtErrorEvent;
+import haxe.CallStack;
+import haxe.io.Path;
 import sys.FileSystem;
 import sys.io.File;
 import sys.io.Process;
@@ -32,8 +33,7 @@ class Main extends Sprite
 	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var initialState:Class<FlxState> = TitleState; // The FlxState the game starts with.
-	#if (flixel < "5.0.0") var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
-        #end
+	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
 	var framerate:Int = 60; // How many frames per second the game should run at.
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
@@ -75,7 +75,6 @@ class Main extends Sprite
 		var stageWidth:Int = Lib.current.stage.stageWidth;
 		var stageHeight:Int = Lib.current.stage.stageHeight;
 
-                #if (flixel < "5.0.0")
 		if (zoom == -1)
 		{
 			var ratioX:Float = stageWidth / gameWidth;
@@ -84,7 +83,6 @@ class Main extends Sprite
 			gameWidth = Math.ceil(stageWidth / zoom);
 			gameHeight = Math.ceil(stageHeight / zoom);
 		}
-                #end
 
 		SUtil.check();
 	
